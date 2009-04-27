04bd6ff2602440d21bacd2a4e449fc4bf5c94abb
Paul Miller <paul@mei.net>
2009-04-27 14:37:32 -0400

    better name maybe?

--- MrSh.pm
+++
@@ -1,6 +0,0 @@
-package APP::MrSh;
-
-use strict;
-use warnings;
-
-our $VERSION = 0.1;
---
+++ MrShell.pm
@@ -0,0 +1,8 @@
+package APP::MrShell;
+
+use strict;
+use warnings;
+use IPC::System::Simple;
+use Term::ReadLine;
+
+our $VERSION = '0.1000';
