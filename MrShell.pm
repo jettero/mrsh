5dbc196d22a65215ec56afd421bb4af95eb0ff53
Paul Miller <jettero@cpan.org>
2009-04-28 06:56:30 -0400

    version 2.0 sans readline shell will probably look like this

--- MrShell.pm
+++ MrShell.pm
@@ -1,9 +1,34 @@
-package APP::MrShell;
+package App::MrShell;
 
 use strict;
 use warnings;
-use IPC::System::Simple;
-use Term::ReadLine;
-use DBM::Deep;
+
+# use Config::Tiny; # this is probably smarter for groups than DBM::Deep
+# use DBM::Deep;
+# use Term::ReadLine;
+
+use IO::Select;
+use IPC::Open3 qw(open3);
 
 our $VERSION = '2.0000';
+
+sub new { bless {} }
+
+sub set_hosts {
+    my $this = shift;
+       $this->{_hosts} = \@_;
+
+    $this;
+}
+
+sub run_command {
+    my $this = shift;
+
+    $this;
+}
+
+sub show_result {
+    my $this = shift;
+
+    0;
+}
